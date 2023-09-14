0a0800715b035e975c56b8e2be1805b040f5761c
Moinuddin-150204014 <141901227+Moinuddin-150204014@users.noreply.github.com>
2023-09-14 16:20:31 +0600

    Final update

---
+++ Online-Book-Library/src/main/java/com/example/Online/Book/Library/entity/BookEntity.java
@@ -0,0 +1,20 @@
+package com.example.Online.Book.Library.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "book")
+public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookId;
+    private String title;
+    private String author;
+    private String genre;
+    private String about;
+    private String status;
+}
